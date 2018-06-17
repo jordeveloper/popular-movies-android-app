8752f6c4f0b82e6cc287349ca3c917d7f2ec96d9
Stefanie Molin <stefaniemolin@gmail.com>
2018-06-16 23:02:59 -0400

    Add radio buttons and enable toggling between the two sort types for the API requests: popular and top rated.

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.example.android.popularmovies.utilities.APIUtils;
@@ -21,6 +22,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Movie>>, MoviePosterAdapter.GridItemClickListener{
     private static final String LOG_TAG = "MainActivity";
+    private final int LOADER_ID = 100;
     private RecyclerView mRecyclerView;
     private MoviePosterAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public static final String MOVIE_POSTER_URL_DATA = "movie poster url";
     public static final String MOVIE_VOTE_AVERAGE_DATA = "vote average";
     public static final String MOVIE_SUMMARY_DATA = "plot synopsis";
+    public String mSortType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mAdapter = new MoviePosterAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
 
+        // set which type of data to pull
+        if (((RadioButton) findViewById(R.id.radio_popular)).isChecked()) {
+            this.mSortType = APIUtils.POPULAR_ENDPOINT;
+        } else {
+            this.mSortType = APIUtils.TOP_RATED_ENDPOINT;
+        }
+
         // Check for internet connection
         ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
@@ -53,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         if (isConnected){
             LoaderManager loaderManager = getLoaderManager();
-            loaderManager.initLoader(100, null, this);
+            loaderManager.initLoader(LOADER_ID, null, this);
             Log.v(LOG_TAG, "Loader created!");
         } else {
             // DONE Hide loading indicator because the data has been loaded
@@ -69,8 +79,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public Loader<List<Movie>> onCreateLoader(int id, Bundle args) {
         // TODO get the sorted preference from the bundle and use that one instead of always POPULAR
-
-        return new MovieAsyncTaskLoader(this, APIUtils.POPULAR_ENDPOINT);
+        return new MovieAsyncTaskLoader(this, this.mSortType);
     }
 
     @Override
@@ -105,5 +114,28 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         intent.putExtra(this.MOVIE_VOTE_AVERAGE_DATA, movie.getVoteAverage());
         intent.putExtra(this.MOVIE_SUMMARY_DATA, movie.getOverview());
         startActivity(intent);
+    }
+
+    // Buttons for which search type
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_popular:
+                if (checked) {
+                    // Get data for popular movies
+                    this.mSortType = APIUtils.POPULAR_ENDPOINT;
+                    getLoaderManager().restartLoader(LOADER_ID, null, this);
+                    break;
+                }
+            case R.id.radio_top_rated:
+                if (checked) {
+                    // Get data for top rated movies
+                    this.mSortType = APIUtils.TOP_RATED_ENDPOINT;
+                    getLoaderManager().restartLoader(LOADER_ID, null, this);
+                    break;
+                }
+        }
     }
 }
